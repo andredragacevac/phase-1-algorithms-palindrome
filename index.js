7eed0c45a03caec9209a6da7026caea98529eaba
Andre Dragacevac <a.dragacevac@icloud.com>
2022-11-07 16:51:54 -0500

    commit tree eea0c00afad646994658146387698a01f76f2232

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  /*const reversedWord = reverse(word);
+  return word === reversedWord;
+}*/
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length -1 -i;
+    if(word[i] !== word[j]) return false;
+  }
+  return true
 }
 
+/*function reverse(word) {
+  //'abc' => 'cba'
+  //const wordArray = word.split("");
+  //const reversedWordArray = wordArray.reverse();
+  //const reversedWord = reversedWordArray.join("");
+  //return reversedWord;
+  return word.split("").reverse().join("");
+}
+*/
 /* 
-  Add your pseudocode here
+make a function that returns true if word is palindrome. that means if the first letter is the same
+as second to last letter etc. until we reach the middle , return true
+
+iterate from the beginning to the middle of the word
+  check each character to the corresponding letter from end
+    if any letters dont match, return false
+return true
 */
 
 /*
